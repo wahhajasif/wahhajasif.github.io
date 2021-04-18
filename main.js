eae880541d73412f743bfb44b34e7137af2fa6a5
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2021-04-18 15:51:55 +0500

    add responsive navbar

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 import images from "./images.js";
 
-const main = document.querySelector("main.container");
-images.forEach((imageName) => {
-  const img = document.createElement("img");
-  img.src = `images/${imageName}`;
-  main.appendChild(img);
-});
+const addImages = () => {
+  const main = document.querySelector("main.container");
+  images.forEach((imageName) => {
+    const img = document.createElement("img");
+    img.src = `images/${imageName}`;
+    main.appendChild(img);
+  });
+};
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
+addImages();
