029622b397b57b0e9dcafc9211a28a7fe203efb8
wahhajasif <82605251+wahhajasif@users.noreply.github.com>
2021-04-22 14:35:38 +0000

    Deploying to gh-pages from @ wahhajasif/wahhajasif.github.io@6549ec09f71cf2a9a631ee722fe54679c32068bb 🚀

--- js/images.js
+++ js/images.js
@@ -1,9 +1,7 @@
 export default [
   "wahhaj-asif-screenshot003.jpg",
-  "wahhaj-asif-screenshot005.jpg",
   "wahhaj-asif-screenshot006.jpg",
   "wahhaj-asif-screenshot012.jpg",
-  "wahhaj-sheikh-blenderdnoise500samplesuntitled2.jpg",
   "wahhaj-sheikh-blenderdnoise500samplesuntitled4.jpg",
   "wahhaj-sheikh-dcar-6.jpg"
 ];
