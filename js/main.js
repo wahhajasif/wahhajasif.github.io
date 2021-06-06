11cae3722a91133ed0bb0f1060361de649ac529d
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2021-06-06 08:42:06 +0000

    Deploying to gh-pages from @ wahhajasif/wahhajasif.github.io@e4041edb3305e9f85c8ef755adb1aa6672cf9b34 🚀

--- js/main.js
+++ js/main.js
@@ -1,23 +1,69 @@
-import images from "./images.js";
+import imageURLs from "./images.js";
 
+const imageURLsByTabName = new Map();
 const addImages = () => {
-  const imageURLs = images.map((imageFilename) => "images/" + imageFilename);
+  const tabsList = new Set();
 
-  const onImgLoad = (img) => {
-    img.classList.remove("invisible");
-  };
+  imageURLs.forEach((element) => {
+    const imageURL = element;
+    const imageTab = element.split("/")[1];
+    tabsList.add(imageTab);
 
-  const onAllImagesLoaded = () => {
-    document.querySelectorAll(".loading-images-message").forEach((e) => {
-      e.classList.toggle("invisible");
-    });
-  };
-
-  loadNextImageAfterFirstImageLoaded(imageURLs, 0, {
-    onAllImagesLoaded: onAllImagesLoaded,
-    imgClassList: [],
-    onImgLoad: onImgLoad,
+    if (imageURLsByTabName[imageTab] == null) {
+      imageURLsByTabName[imageTab] = [imageURL];
+    } else {
+      imageURLsByTabName[imageTab].push(imageURL);
+    }
   });
+  console.log(imageURLsByTabName);
+  const tabsNavBar = document.querySelector(".tabs-bar");
+
+  Object.entries(imageURLsByTabName).forEach((entry, index) => {
+    const tabDiv = document.createElement("div");
+
+    const tabName = entry[0];
+    const imageURLs = entry[1];
+
+    tabDiv.classList.add("tab");
+    tabDiv.innerText = tabName;
+    tabDiv.setAttribute("data-tabname", tabName);
+
+    // make the first tab selected
+    if (index == 0) tabDiv.classList.add("selected");
+
+    tabsNavBar.appendChild(tabDiv);
+
+    const main = document.querySelector("main");
+    const tabContent = document.createElement("div");
+    tabContent.classList.add("photos-container");
+    tabContent.setAttribute("data-tabname", tabName);
+    if (index != 0) tabContent.style.display = "none";
+
+    imageURLs.forEach((imageURL) => {
+      console.log(`adding image ${imageURL}}`);
+      const img = document.createElement("img");
+      img.src = imageURL;
+      tabContent.appendChild(img);
+    });
+
+    main.appendChild(tabContent);
+  });
+
+  // const onImgLoad = (img) => {
+  //   img.classList.remove("invisible");
+  // };
+
+  // const onAllImagesLoaded = () => {
+  //   document.querySelectorAll(".loading-images-message").forEach((e) => {
+  //     e.classList.toggle("invisible");
+  //   });
+  // };
+
+  // loadNextImageAfterFirstImageLoaded(imageURLs, 0, {
+  //   onAllImagesLoaded: onAllImagesLoaded,
+  //   imgClassList: [],
+  //   onImgLoad: onImgLoad,
+  // });
 };
 
 function loadNextImageAfterFirstImageLoaded(
@@ -61,7 +107,39 @@ const navSlide = () => {
   });
 };
 
+const initializeTabsBarHandler = () => {
+  const mainTabBar = document.querySelector("div.tabs-bar");
+  const allTabs = mainTabBar.querySelectorAll(".tab");
+
+  Object.keys(imageURLsByTabName).forEach((tabName, index) => {
+    const tabDiv = mainTabBar.querySelector(
+      `div.tab[data-tabname="${tabName}"]`
+    );
+
+    tabDiv.addEventListener("click", (e) => {
+      allTabs.forEach((itab) => {
+        itab.classList.remove("selected");
+
+        Object.keys(imageURLsByTabName).forEach((tabName, index) => {
+          document.querySelector(
+            `.photos-container[data-tabname="${tabName}"]`
+          ).style.display = "none";
+        });
+      });
+
+      tabDiv.classList.toggle("selected");
+
+      document.querySelector(
+        `.photos-container[data-tabname="${tabName}"]`
+      ).style.display = "";
+    });
+  });
+
+  allTabs.forEach((tab) => {});
+};
+
 window.onload = () => {
   navSlide();
   addImages();
+  initializeTabsBarHandler();
 };
